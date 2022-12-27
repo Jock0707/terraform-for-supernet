4102c498d5f562526a8c5781b84f744d1d0e18b3
Jock0707 <94595117+Jock0707@users.noreply.github.com>
2022-12-27 07:45:02 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "instance_id" {
+output "instance_id" {bc1qshflv83stdd49rw3w5728kuke0wj7alhxadm2v
   description = "The name of the buckets."
-  value       = {
+  value       = {15000
     for k, v in module.ec2_instances_to_existed_vpc :k => v.id
   }
 }
